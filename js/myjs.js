4f2277d9e2ef2fa8adc66c9492f8872b682c6c91
Re7miguel <re7miguel@gmail.com>
2017-03-09 10:41:17 -0300

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -28,3 +28,34 @@ function Botao(professor) {
   }
     document.getElementById(professor).style.display = "block";
 }
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function filterFunction2() {
+    var input, filter, ul, li, p, i;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown2");
+    p = div.getElementsByTagName("p");
+    for (i = 0; i < p.length; i++) {
+        if (p[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            p[i].style.display = "";
+        } else {
+            p[i].style.display = "none";
+        }
+    }
+}
+
+function Botao2(turma) {
+  var classes = document.getElementsByClassName("renan2");
+  var i, davez;
+  for (i = 0; i < classes.length; i++){
+    davez = classes[i];
+    if (davez.style.display != "none"){
+      document.getElementById(davez.id).style.display = "none";
+    }
+  }
+    document.getElementById(turma).style.display = "block";
+}
